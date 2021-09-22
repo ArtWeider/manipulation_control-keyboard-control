23fea933e8d4927aa0cb06e4ec8f27eb77245917
MrKanYT <mrkan2017@gmail.com>
2021-09-22 20:13:02 +0400

    Добавлены остальные файлы

---
+++ main.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from UpperStatusBarWidget import UpperStatusBar
+from SavesWidget import SavesWidget
+from config import Cfg as cfg
+from tkinter import ttk
+from PIL import Image, ImageTk
+from PointMenuWidget import PointMenuWidget
+from HandVisualisationWidget import HandVisualisationWidget
+from SavesMenuWidget import SavesMenuWidget
+from MatplotlibWidget import MatplotlibWidget
+from TimelineWidget import TimelineWidget
+from XYVisualisationWidget import XYVisualisationWidget
+from XZVisualisationWidget import XZVisualisationWidget
+from ControlPanelWidget import ControlPanelWidget
+
+
+class Main:
+
+    root = None
+    style = None
+
+    upperStatusBar = None
+    savesWidget = None
+    pointMenuWidget = None
+    handVisualisationWidget = None
+    savesMenuWidget = None
+    matplotlibWidget = None
+    timelineWidget = None
+    xyVisualisationWidget = None
+    xzVisualisationWidget = None
+    controlPanelWidget = None
+
+    def __init__(self):
+        self.root = Tk()
+        self.style = ttk.Style()
+
+        _image = Image.open("Images/RoundedImage.png")
+        borderImage = ImageTk.PhotoImage(_image)
+        self.style.element_create("RoundedFrame",
+                     "image", borderImage,
+                     border=16, sticky="nsew")
+        self.style.layout("RoundedFrame",
+                     [("RoundedFrame", {"sticky": "nsew"})])
+
+        self.root.title(cfg.WINDOW_NAME)
+        self.root.geometry(cfg.SIZE)
+        self.root.resizable(width=False, height=False)
+
+        self.root.configure(bg=cfg.MAIN_COLOR)
+
+        self.upperStatusBar = UpperStatusBar()
+        self.savesWidget = SavesWidget()
+        self.pointMenuWidget = PointMenuWidget()
+        self.handVisualisationWidget = HandVisualisationWidget()
+        self.savesMenuWidget = SavesMenuWidget()
+        self.matplotlibWidget = MatplotlibWidget(self.root)
+        self.timelineWidget = TimelineWidget(self.root)
+        self.xyVisualisationWidget = XYVisualisationWidget()
+        self.xzVisualisationWidget = XZVisualisationWidget()
+        self.controlPanelWidget = ControlPanelWidget()
+
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    main = Main()
