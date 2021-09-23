9efa2f4901e4efa6587d491c1b1d2264c8b78baa
ArtWeider <68417438+ArtWeider@users.noreply.github.com>
2021-09-23 07:47:03 +0400

    Update TimelineWidget.py

--- TimelineWidget.py
+++ TimelineWidget.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from config import Cfg as cfg
-
+from math import *
 
 class TimelineWidget:
 
@@ -14,8 +14,18 @@ class TimelineWidget:
     WIDTH = 800 * cfg.SIZE_MULT
     HEIGHT = 160 * cfg.SIZE_MULT
 
+    scrollX = 0
+
     def _on_mousewheel(self, event):
-        self.canvas.xview_scroll(-1 * int(event.delta / 120), "units")
+        self.canvas.xview_scroll(int(copysign(1, -event.delta)), "units")
+        self.scrollX += copysign(ceil(self.canvas.bbox("all")[2] / 60), -event.delta)
+
+        if self.scrollX < 0:
+            self.scrollX = 0
+        elif self.scrollX > 2450:
+            self.scrollX = 2450
+
+        print(self.scrollX)
 
     def __init__(self, root):
 
@@ -28,24 +38,25 @@ class TimelineWidget:
                              width=self.WIDTH - 20,
                              height=self.HEIGHT - 20,
                              bg=cfg.MAIN_COLOR,
-                             highlightthickness=0,
+                             highlightthickness=0
                              )
         self.canvas.place(x=10, y=10)
+
+        # self.canvas.xscrollincrement = self.canvas.bbox("all")[2]
+
         self.canvas.bind("<MouseWheel>", self._on_mousewheel)
+        self.canvas.bind("<Double-Button-1>", self.create_point)
 
         for i in range(60):
             self.canvas.create_line(i*50, 30, i*50, 90, width=2, fill=cfg.LINE_COLOR)
             self.canvas.create_text(i*50, 10, text=str(i+1), fill=cfg.LINE_COLOR)
 
-        def create_point(x):
-            y = 60
-            self.canvas.create_polygon((x, y+5), (x+5, y), (x, y-5), (x-5, y), fill='orange')
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"), xscrollincrement=ceil(self.canvas.bbox("all")[2] / 60))
 
-        create_point(25)
-        create_point(178)
-        create_point(61)
-
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+    def create_point(self, event):
+        x = (self.scrollX + (event.widget.winfo_pointerx() - event.widget.winfo_rootx()))
+        y = 60
+        self.canvas.create_polygon((x, y+5), (x+5, y), (x, y-5), (x-5, y), fill='orange')
 
 
 
