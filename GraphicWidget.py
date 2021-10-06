a6c3b5731ae2e4e3f18118c05f3c5428b6356ee7
ArtWeider <68417438+ArtWeider@users.noreply.github.com>
2021-10-06 19:09:55 +0400

    Update GraphicWidget.py

--- GraphicWidget.py
+++ GraphicWidget.py
@@ -5,8 +5,10 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 def cm2inch(sm):
     return sm * 0.39
+
 
 class GraphicWidget:
     X = 515 * Cfg.SIZE_MULT
@@ -17,6 +19,8 @@ class GraphicWidget:
     def quit(self):
         self.root.remove()
         exit()
+
+        print(self.main.graphicWidget.point.points)
 
     class Create3DPlot:
         def __init__(self, root):
@@ -34,11 +38,12 @@ class GraphicWidget:
             figure.canvas.mpl_disconnect(figure.canvas.manager.key_press_handler_id)
             canvas = FigureCanvasTkAgg(figure, root)
             canvas.draw()
-            canvas.get_tk_widget().place(x=GraphicWidget.X+11, y=GraphicWidget.Y+15)
+            canvas.get_tk_widget().place(x=GraphicWidget.X + 11, y=GraphicWidget.Y + 15)
 
     class CreatePoint:
 
         setPointFlag = False
+        updateScreenFlag = True
         AxesSphereMovementFlag = False
         ShiftFlag = False
         CtrlFlag = False
@@ -58,7 +63,29 @@ class GraphicWidget:
             root.bind("<KeyPress>", self.place_point)
             root.bind("<KeyRelease>", self.changing_flags)
 
-        def dictionaryUpdate(self, flag, values=(0, 0, 0, 0, 0, 0, 0, 0)):
+        def dictUpdate(self):
+            save = self.main.savesManager.currentSave
+
+            self.points['x'] = []
+            self.points['y'] = []
+            self.points['z'] = []
+            self.points['time'] = []
+
+            if len(self.main.savesManager.saves[save].points) != len(self.points['x']):
+                for time in self.main.savesManager.saves[save].points.keys():
+                    self.points['x'].append(self.main.savesManager.saves[save].points[time].x)
+                    self.points['y'].append(self.main.savesManager.saves[save].points[time].y)
+                    self.points['z'].append(self.main.savesManager.saves[save].points[time].z)
+                    self.points['time'].append(time)
+                    self.points['rad'].append(0)
+                    self.points['a'].append(0)
+                    self.points['b'].append(0)
+                    self.points['c'].append(0)
+
+            self.updateScreenFlag = True
+            print(1)
+
+        def dictEdit(self, flag, values=(0, 0, 0, 0, 0, 0, 0, 0)):
             if flag:
                 self.points['time'].append(values[0])
                 self.points['x'].append(values[1])
@@ -72,6 +99,8 @@ class GraphicWidget:
                 for key in self.points.keys():
                     self.points[key].pop()
 
+            self.main.graphicWidget.updateScreenFlag = True
+
         def changing_flags(self, event):
             if event.keysym == 'r':
                 self.AxesSphereMovementFlag = not self.AxesSphereMovementFlag
@@ -81,8 +110,10 @@ class GraphicWidget:
 
                 if not self.CtrlFlag and self.setPointFlag:
                     for i in range(self.cornerNum):
-                        self.dictionaryUpdate(False)
+                        self.dictEdit(False)
                     self.cornerNum = 0
+
+            self.main.graphicWidget.updateScreenFlag = True
 
         def getSphericalCoordinates(self, a):
             try:
@@ -156,10 +187,10 @@ class GraphicWidget:
             elif event.keysym == 'e':
                 self.params['z'] -= 5
 
-                self.points['rad'][-1] = 0
-                self.points['a'][-1] = 0
-                self.points['b'][-1] = 0
-                self.points['c'][-1] = 0
+            self.points['rad'][-1] = 0
+            self.points['a'][-1] = 0
+            self.points['b'][-1] = 0
+            self.points['c'][-1] = 0
 
         def placing_polygon(self, event):
             if self.CtrlFlag:
@@ -168,7 +199,7 @@ class GraphicWidget:
                     if self.cornerNum > 10:
                         self.cornerNum = 10
                     else:
-                        self.dictionaryUpdate(True)
+                        self.dictEdit(True)
 
                 elif event.keysym == 'minus':
 
@@ -176,7 +207,7 @@ class GraphicWidget:
                     if self.cornerNum < 0:
                         self.cornerNum = 0
                     else:
-                        self.dictionaryUpdate(False)
+                        self.dictEdit(False)
 
                 for i in range(1, self.cornerNum + 1):
                     self.points['x'][-i - 1], self.points['y'][-i - 1], self.points['z'][-i - 1] = \
@@ -204,6 +235,9 @@ class GraphicWidget:
                 else:
                     self.axes_movement(event)
 
+                if event.keysym == 's' or event.keysym == 'd' or event.keysym == 'w' or event.keysym == 'a' or event.keysym == 'q' or event.keysym == 'e' or event.keysym == 'z' or event.keysym == 'c':
+                    self.main.pointMenuWidget.onPointSelected(self.selectedTime)
+
                 # диапазоны
                 if self.params['x'] > self.max:
                     self.params['x'] = self.max
@@ -221,9 +255,12 @@ class GraphicWidget:
                     self.params['z'] = self.min + 200
 
                 # присваивание координат точке
-                self.points['x'][-1] = self.params['x']
-                self.points['y'][-1] = self.params['y']
-                self.points['z'][-1] = self.params['z']
+                for i in range(len(self.points['time'])):
+                    if self.points['time'][i] == self.selectedTime:
+                        self.points['x'][i] = self.params['x']
+                        self.points['y'][i] = self.params['y']
+                        self.points['z'][i] = self.params['z']
+                        break
 
                 current_point = self.main.savesManager.saves[save].points[self.selectedTime]
 
@@ -231,86 +268,91 @@ class GraphicWidget:
                 self.main.savesManager.saves[save].points[self.selectedTime].y = self.params['y']
                 self.main.savesManager.saves[save].points[self.selectedTime].z = self.params['z']
 
-                self.main.pointMenuWidget.onPointSelected(self.selectedTime)
-
-                print(self.main.savesManager.saves[save].points[self.selectedTime].x)
+                self.updateScreenFlag = True
 
     def drawObjects(self, frame):
-        self.plot3d.ax_3d.clear()
+        if self.point.updateScreenFlag:
+            self.plot3d.ax_3d.clear()
+            print(self.point.setPointFlag)
 
-        self.plot3d.ax_3d.plot3D(self.point.points['x'], self.point.points['y'], self.point.points['z'], color='orange', marker='o')
-        self.plot3d.ax_3d.set_xlim(xmax=self.point.max, xmin=self.point.min)
-        self.plot3d.ax_3d.set_ylim(ymax=self.point.min, ymin=self.point.max)
-        self.plot3d.ax_3d.set_zlim(zmax=self.point.max + 200, zmin=self.point.min + 200)
+            self.plot3d.ax_3d.plot3D(self.point.points['x'], self.point.points['y'], self.point.points['z'], color='orange', marker='o')
+            self.plot3d.ax_3d.set_xlim(xmax=self.point.max, xmin=self.point.min)
+            self.plot3d.ax_3d.set_ylim(ymax=self.point.min, ymin=self.point.max)
+            self.plot3d.ax_3d.set_zlim(zmax=self.point.max + 200, zmin=self.point.min + 200)
 
-        plt.xlabel("X")
-        plt.ylabel("Y")
+            plt.xlabel("X")
+            plt.ylabel("Y")
 
-        if len(self.point.points['x']) > 1:
+            if len(self.point.points['x']) > 1:
 
-            x1 = self.point.points['x'][-1]
-            x2 = self.point.points['x'][-2]
+                x1 = self.point.points['x'][-1]
+                x2 = self.point.points['x'][-2]
 
-            y1 = self.point.points['y'][-1]
-            y2 = self.point.points['y'][-2]
+                y1 = self.point.points['y'][-1]
+                y2 = self.point.points['y'][-2]
 
-            z1 = self.point.points['z'][-1]
-            z2 = self.point.points['z'][-2]
+                z1 = self.point.points['z'][-1]
+                z2 = self.point.points['z'][-2]
 
-            if self.point.setPointFlag:
+                if self.point.setPointFlag:
 
-                if self.point.AxesSphereMovementFlag:
-                    a = np.linspace(0, 360, 50)
+                    if self.point.AxesSphereMovementFlag:
+                        a = np.linspace(0, 360, 50)
 
-                    x, y, z = self.point.getSphericalCoordinates(a)
+                        x, y, z = self.point.getSphericalCoordinates(a)
 
-                    self.plot3d.ax_3d.plot3D(x, y, z, color='gray', linestyle=':')
+                        self.plot3d.ax_3d.plot3D(x, y, z, color='gray', linestyle=':')
 
-                if abs(z1 - z2) < 15 and abs(y1 - y2) < 15:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='red', linestyle=':')
+                    if abs(z1 - z2) < 15 and abs(y1 - y2) < 15:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='red', linestyle=':')
 
-                    if self.point.ShiftFlag:
-                        self.point.points['y'][-1] = y2
-                        self.point.params['y'] = y2
-                        self.point.points['z'][-1] = z2
-                        self.point.params['z'] = z2
+                        if self.point.ShiftFlag:
+                            self.point.points['y'][-1] = y2
+                            self.point.params['y'] = y2
+                            self.point.points['z'][-1] = z2
+                            self.point.params['z'] = z2
 
-                if abs(z1 - z2) < 15 and abs(x1 - x2) < 15:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='green', linestyle=':')
+                    if abs(z1 - z2) < 15 and abs(x1 - x2) < 15:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='green', linestyle=':')
 
-                    if self.point.ShiftFlag:
-                        self.point.points['x'][-1] = x2
-                        self.point.params['x'] = x2
-                        self.point.points['z'][-1] = z2
-                        self.point.params['z'] = z2
+                        if self.point.ShiftFlag:
+                            self.point.points['x'][-1] = x2
+                            self.point.params['x'] = x2
+                            self.point.points['z'][-1] = z2
+                            self.point.params['z'] = z2
 
-                if abs(x1 - x2) < 15 and abs(y1 - y2) < 15:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='blue', linestyle=':')
+                    if abs(x1 - x2) < 15 and abs(y1 - y2) < 15:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='blue', linestyle=':')
 
-                    if self.point.ShiftFlag:
-                        self.point.points['x'][-1] = x2
-                        self.point.params['x'] = x2
-                        self.point.points['y'][-1] = y2
-                        self.point.params['y'] = y2
+                        if self.point.ShiftFlag:
+                            self.point.points['x'][-1] = x2
+                            self.point.params['x'] = x2
+                            self.point.points['y'][-1] = y2
+                            self.point.params['y'] = y2
 
-            for i in range(len(self.point.points['x'])):
-                x1 = self.point.points['x'][i - 1]
-                x2 = self.point.points['x'][i]
+                for i in range(len(self.point.points['x'])):
+                    x1 = self.point.points['x'][i - 1]
+                    x2 = self.point.points['x'][i]
 
-                y1 = self.point.points['y'][i - 1]
-                y2 = self.point.points['y'][i]
+                    y1 = self.point.points['y'][i - 1]
+                    y2 = self.point.points['y'][i]
 
-                z1 = self.point.points['z'][i - 1]
-                z2 = self.point.points['z'][i]
+                    z1 = self.point.points['z'][i - 1]
+                    z2 = self.point.points['z'][i]
 
-                if abs(z1 - z2) == 0 and abs(y1 - y2) == 0:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='red', marker='o')
+                    if abs(z1 - z2) == 0 and abs(y1 - y2) == 0:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='red', marker='o')
 
-                if abs(z1 - z2) == 0 and abs(x1 - x2) == 0:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='green', marker='o')
+                    if abs(z1 - z2) == 0 and abs(x1 - x2) == 0:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='green', marker='o')
 
-                if abs(x1 - x2) == 0 and abs(y1 - y2) == 0:
-                    self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='blue', marker='o')
+                    if abs(x1 - x2) == 0 and abs(y1 - y2) == 0:
+                        self.plot3d.ax_3d.plot3D((x1, x2), (y1, y2), (z1, z2), color='blue', marker='o')
+
+                    if self.point.setPointFlag:
+                        self.plot3d.ax_3d.plot3D(self.point.params['x'], self.point.params['y'], self.point.params['z'], color='OrangeRed', marker='x')
+
+            self.point.updateScreenFlag = False
 
     def __init__(self, main):
         self.main = main
