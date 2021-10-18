bb6cfa1a0ae0a03d55858e81e8cc37b61375ddc5
ArtWeider <68417438+ArtWeider@users.noreply.github.com>
2021-10-18 14:36:24 +0400

    Update SaveClass.py

--- SaveClass.py
+++ SaveClass.py
@@ -1,6 +1,4 @@
 from PointClass import Point
-import json
-import tkinter as tk
 
 class Save:
 
@@ -16,18 +14,24 @@ class Save:
             point.fromDict(i)
             self.points[point.time] = point
 
-
     def toDict(self):
         return {'name': self.name, 'points': list(map(Point.toDict, self.points.values()))}
 
-    def addPoint(self, time, x=0, y=0, z=0, q=0, e=0, f=0):
+    def addPoint(self, time, x=0, y=0, z=0, rad=0, a=0, b=0, c=0, q=0, e=0, f=0):
         point = Point()
         point.time = time
+
         point.x = x
         point.y = y
         point.z = z
+
+        point.rad = rad
+        point.a = a
+        point.b = b
+        point.c = c
+
         point.q = q
         point.e = e
         point.f = f
-        self.points[point.time] = point
 
+        self.points[point.time] = point
